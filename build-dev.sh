41d1639ecf6b056d3e45e40253e4e9862e9d53bb
Nicolás Agustín <84692683+nicoagustinn@users.noreply.github.com>
2021-09-12 00:24:54 -0300

    Update build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -6,6 +6,5 @@
 # as noticed, there's no sudo. I removed anything that required root permission
 # because I was lazy to enter my password :D
 
-./version-check.sh || { echo 'version-check failed, verify you have all required development tools.' ; exit 1; }
 gcc -v perla-install.c -o perla-install;
 gcc -v perla-build.c -o perla-build;
