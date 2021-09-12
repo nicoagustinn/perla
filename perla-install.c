1abe65903ffe71110179227f1c1c33e9cfe8985d
Nicolás Agustín <84692683+nicoagustinn@users.noreply.github.com>
2021-09-11 23:33:57 -0300

    Update perla-install.c

--- perla-install.c
+++ perla-install.c
@@ -1,3 +1,4 @@
+
 /*
 	█ ▄▄  ▄███▄   █▄▄▄▄ █    ██   
 	█   █ █▀   ▀  █  ▄▀ █    █ █  
@@ -10,10 +11,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 const char base[]= "base";
 
-const char *command = "python3 perla-build.py";
+const char *command = "./perla-build";
 
 int main(int argc, char *argv[]) {
 main:
@@ -34,5 +34,3 @@ main:
 	}
 	return 0;
 }
-
-
