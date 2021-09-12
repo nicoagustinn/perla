dd748cc6a3255135ca7bf46294d2fb505c372036
Nicolás Agustín <84692683+nicoagustinn@users.noreply.github.com>
2021-09-12 00:20:36 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 ./version-check.sh || { echo 'version-check failed, verify you have all required development tools.' ; exit 1; }
 gcc -v perla-install.c -o perla-install;
 gcc -v perla-build.c -o perla-build;
+sudo cp -v perla-install /usr/local/bin/perla-install;
+sudo cp -v perla-build /usr/local/bin/perla-build;
